0ae1984e8791a27fc656d2e20c7610c00110349e
Super Nani <elaine.programadora@gmail.com>
2025-01-27 19:01:48 -0300

    Segregação campo minado em subclasses

---
+++ jogos/campo_minado/campo_minado_1facil.py
@@ -0,0 +1,6 @@
+from jogos.campo_minado.campo_minado import Campo_minado
+
+
+class CampoMinadoFacil(Campo_minado):
+    def __init__(self):
+        super().__init__()
---
+++ jogos/campo_minado/client.py
@@ -0,0 +1,6 @@
+from jogos.campo_minado.construtor_campo_minado import ConstrutorCampoMinado
+
+
+def main():
+    construtor = ConstrutorCampoMinado()
+    campo_usuario = construtor.escolhe_campo()
